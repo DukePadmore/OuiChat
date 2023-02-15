9e87fbcbc5778952d9bb0a0a962ade8a2116a33e
Duke Padmore <dukepadmore@gmail.com>
2023-02-15 14:11:47 -0500

    Edit styles on Sidebar component

--- src/components/Convos.jsx
+++ src/components/Convos.jsx
@@ -34,6 +34,114 @@ const Convos = () => {
         </div>
         <div className='convo-details'>
           <h3>Minion</h3>
+          <p>Salut! Comment tu vas ? Moi ça va nickel sauf que blablablabla</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
           <p>Salut! Comment tu vas ?</p>
         </div>
       </section>
@@ -145,6 +253,19 @@ const Convos = () => {
           <p>Salut! Comment tu vas ?</p>
         </div>
       </section>
+      <section className='convo-item'>
+        <div className='user-img'>
+          <img
+            src='https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            alt='Minion'
+          />
+        </div>
+        <div className='convo-details'>
+          <h3>Minion</h3>
+          <p>Salut! Comment tu vas ?</p>
+        </div>
+      </section>
+      <p className='convos-end'>Start a new conversation !</p>
     </div>
   );
 };
