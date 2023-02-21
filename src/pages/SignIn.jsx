783f6448770e70551ee04836b5cbba1cf127d199
Duke Padmore <dukepadmore@gmail.com>
2023-02-21 16:21:43 -0500

    Finish basic design

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,23 @@
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className='container'>
+      <div className='signin'>
+        <h2>Sign in to your account</h2>
+        <form className='signin-form'>
+          <div className='form-inputs'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' />
+          </div>
+          <div className='form-inputs'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' />
+          </div>
+          <button className='signin-submit'>Sign in</button>
+        </form>
+        <p>Don't have an account? Sign up</p>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,19 +1,31 @@
+import Img from '../assets/images/image.svg';
+
 const SignUp = () => {
   return (
     <div className='container'>
       <div className='signup'>
-        <h2>Create your OuiChat account</h2>
+        <h2>Create your account</h2>
         <form className='signup-form'>
-          <label htmlFor='username'>Username</label>
-          <input type='text' id='username' />
-
-          <label htmlFor='email'>Email</label>
-          <input type='email' id='email' />
-          <input type='password' id='password' />
-          <input type='file' />
-          <button>Sign up</button>
+          <div className='form-inputs'>
+            <label htmlFor='username'>Username</label>
+            <input type='text' id='username' />
+          </div>
+          <div className='form-inputs'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' />
+          </div>
+          <div className='form-inputs'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' />
+          </div>
+          <label className='image-upload-label' htmlFor='profile-pic'>
+            <img src={Img} alt='image upload icon' />
+            <span>Add a profile picture</span>
+          </label>
+          <input type='file' id='profile-pic' />
+          <button className='signup-submit'>Sign up</button>
         </form>
-        <p>You already have an account ? Sign in</p>
+        <p>Already have an account? Sign in</p>
       </div>
     </div>
   );
